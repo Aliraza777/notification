09b439affc68b972380df18f97d5786005df4dcf
aliraza777 <aliwahlah777@gmail.com>
2022-01-20 19:06:51 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,138 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:notification/model/pushNotification_model.dart';
+import 'package:notification/notification_badge.dart';
+import 'package:overlay_support/overlay_support.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return OverlaySupport(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Push Notification',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late final FirebaseMessaging _messaging;
+  late int _totalNotificationCounter;
+  PushNotification? _notificationInfo;
+
+  void registerNotification() async {
+    await Firebase.initializeApp();
+
+    _messaging = FirebaseMessaging.instance;
+    print("testing......");
+    NotificationSettings settings = await _messaging.requestPermission(
+      alert: true,
+      badge: true,
+      provisional: false,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print("User granted the permission");
+
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+        PushNotification notification = PushNotification(
+          title: message.notification!.title,
+          body: message.notification!.body,
+          dataTitle: message.data['title'],
+          dataBody: message.data['body'],
+        );
+        setState(() {
+          _totalNotificationCounter++;
+          _notificationInfo = notification;
+        });
+
+        if (notification != null) {
+          showSimpleNotification(Text(_notificationInfo!.title!),
+              leading: NotificationBadge(
+                  totalNotification: _totalNotificationCounter),
+              subtitle: Text(_notificationInfo!.body!),
+              background: Colors.cyan.shade600,
+              duration: Duration(seconds: 3));
+        }
+      });
+    } else {
+      print("User did not grant permission");
+    }
+  }
+
+  @override
+  void initState() {
+    registerNotification();
+    _totalNotificationCounter = 0;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Push Notification"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Flutter Push Notification",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            NotificationBadge(totalNotification: _totalNotificationCounter),
+            _notificationInfo != null
+                ? Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Title : ${_notificationInfo!.dataTitle ?? _notificationInfo!.title}",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "Body : ${_notificationInfo!.dataBody ?? _notificationInfo!.body}",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                      )
+                    ],
+                  )
+                : Container()
+          ],
+        ),
+      ),
+    );
+  }
+}
